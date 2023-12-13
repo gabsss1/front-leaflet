917cfa79c048c31edaa6e840e8a5991789f94c51
gabsss1 <202011422@urp.edu.pe>
2023-12-13 13:47:28 -0500

    first commit

---
+++ src/components/MapView.js
@@ -0,0 +1,83 @@
+import React, { useState } from 'react';
+import L from 'leaflet';
+import { MapContainer, TileLayer, FeatureGroup } from 'react-leaflet';
+import { EditControl } from 'react-leaflet-draw';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-draw/dist/leaflet.draw.css';
+import { createZona } from '../service/zonaService';
+import Swal from 'sweetalert2'
+
+//Marker Icon fix
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
+  iconUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-icon.png',
+  shadowUrl:
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
+});
+
+
+const MapView = () => {
+  const ZOOM_LEVEL = 5;
+  const LATITUDE = -8.60057858288449;
+  const LONGITUDE = -74.96387566349499;
+
+  const [pendingGeometries, setPendingGeometries] = useState([]);
+
+  const _created = (e) => {
+    const { layer } = e;
+    const geoJSON = layer.toGeoJSON();
+    const { type, coordinates } = geoJSON.geometry;
+
+    setPendingGeometries((prevGeometries) => [
+      ...prevGeometries,
+      { type, coordinates },
+    ]);
+  };
+
+  const saveGeometries = async () => {
+    try {
+      const createdZona = await createZona({
+        name: 'Nombre de la zona',
+        features: pendingGeometries.map((geometry) => ({ geometry })),
+      });
+
+      Swal.fire({
+        title: '¡Buen trabajo!',
+        text: 'Geometría creada',
+        icon: 'success',
+      });
+      setPendingGeometries([]);
+    } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error al crear la geometría',
+            text: '¡Algo salió mal!',
+          });
+    }
+  };
+
+  return (
+    <div style={{ position: 'relative', height: '100vh' }}>
+      <MapContainer center={[LATITUDE, LONGITUDE]} zoom={ZOOM_LEVEL}>
+        <TileLayer
+          url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+
+        <FeatureGroup>
+          <EditControl position='topright' onCreated={_created}/>
+        </FeatureGroup>
+      </MapContainer>
+      
+      <button className="btn secondary" style={{ position: 'absolute', top: '100px', right: '1440px', zIndex: 1000,}}
+        onClick={saveGeometries}>
+        Save
+      </button>
+    </div>
+  );
+};
+
+export default MapView;
---
+++ src/service/zonaService.js
@@ -0,0 +1,50 @@
+// zonaService.js
+
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:3000'; // Reemplaza con la URL de tu backend
+
+export const getAllZonas = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/zona`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error getting all zonas');
+  }
+};
+
+export const getZonaById = async (id) => {
+  try {
+    const response = await axios.get(`${BASE_URL}/zona/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error getting zona by ID');
+  }
+};
+
+export const createZona = async (zonaData) => {
+  try {
+    const response = await axios.post(`${BASE_URL}/zona`, zonaData);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error creating zona');
+  }
+};
+
+export const updateZona = async (id, zonaData) => {
+  try {
+    const response = await axios.put(`${BASE_URL}/zona/${id}`, zonaData);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error updating zona');
+  }
+};
+
+export const deleteZona = async (id) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/zona/${id}`);
+    return response.data;
+  } catch (error) {
+    throw new Error('Error deleting zona');
+  }
+};
